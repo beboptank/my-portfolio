bd0f74fbf3f8f0d82d30d37c0dc30b52622d0620
beboptank <beboptank12@gmail.com>
2020-02-17 11:30:47 +0900

    add Contact component and dummy info

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import ProjectShowcase from './components/ProjectShowcase/ProjectShowcase';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
         <ProjectShowcase />
         <ProjectShowcase />
         <ProjectShowcase />
-        {/*<Contact />
-        <SocialMedia />
+        <Contact />
+        {/*<SocialMedia />
       */}
     </div>
   );
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <section>
+            <div>
+                <h2>
+                    Contact
+        </h2>
+                <div>
+                    <p>
+                        [Put your call to action here]
+          </p>
+                    <a
+                        target="_blank"
+                        class="cta-btn cta-btn--resume"
+                        href="mailto:example@email.com"
+                    >Contact Me</a>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Contact;
