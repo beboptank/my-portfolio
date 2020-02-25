aa340128cae1299660dc88c5b269aef8bcd3ed4e
beboptank <beboptank12@gmail.com>
2020-02-25 14:59:08 +0900

    add styling to project showcase component

--- src/components/ProjectShowcase/ProjectShowcase.js
+++ src/components/ProjectShowcase/ProjectShowcase.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
+import '../../sass/components/_projectshowcase.scss';
 
-
-const ProjectShowcase = ({ name, projectPic, description, source, live }) => {
+const ProjectShowcase = ({ name, description, source, live }) => {
     return (
         <div id="projects" className="projectcontainer">
             <h1 className="projectcontainer__name">{name}</h1>
-            <img className="projectcontainer__image" alt='project' src={projectPic} />
+            <div className="projectcontainer__buttons">
+                <a href={source} className="projectcontainer__buttons__linkbutton">Source Code</a>
+                <a href={live} className="projectcontainer__buttons__linkbutton">View Live</a>
+            </div>
             <div className="projectcontainer__description">
                 <p>
                     {description}
                 </p>
-                <a href={source} className="projectcontainer__button">Source Code</a>
-                <a href={live} className="projectcontainer__button">View Live</a>
             </div>
         </div>
     );
@@ -22,7 +23,6 @@ export default ProjectShowcase;
 
 ProjectShowcase.defaultProps = {
     name: "Cool project",
-    projectPic: 'https://4.bp.blogspot.com/-WL7pR1_DSfE/W0hzY0Sk9rI/AAAAAAAAASQ/doY8xUE3FYkgpWuMzdhv9aARj7qiAsiVwCPcBGAYYCw/s1600/Basset-Hound-pup.jpg',
     description: 'Sick project, bro!',
     source: "www.google.com",
     live: "www.google.com"
