e6adde9218864dc718434751a63fb3c70c59cacf
beboptank <beboptank12@gmail.com>
2020-03-16 12:53:13 +0900

    change favicon, change font styles for headers, add info to projects

--- src/App.js
+++ src/App.js
@@ -9,21 +9,32 @@ import Footer from './components/Footer/Footer';
 import './App.scss';
 import './sass/main.scss';
 
-import safe from './safe.png';
-
 function App() {
   return (
     <div className="App layout">
       
         <Hero />
         <About />
-        <ProjectShowcase />
-        <ProjectShowcase />
+        <ProjectShowcase 
+          name="Reddit Clone"
+          description="An international, open-source team project focused on 
+          recreating the website Reddit. Assisted in developing React components 
+          for the front end. Used Next.js, React, and SCSS."
+          source="https://github.com/chingu-voyages/v13-bears-team-04"
+          live="https://coolredditclone.now.sh/"
+        />
+        <ProjectShowcase 
+          name="Woody"
+          description="Developed and deployed a new website for Woody, a bar in
+          Fukui, Japan. Collaborated with the owner to create a website that
+          reflects the style of his bar. Used Gatsby, React, and SCSS."
+          source="https://github.com/beboptank/woody-website"
+          live="http://woody.surge.sh/"
+        />
         <ProjectShowcase 
           name="SAFE Gardening"
-          projectPic={safe}
-          description="An international team project that focused on redesigning 
-          the website of SAFE Gardening, a nonprofit. We used HTML, CSS, and JavaScript
+          description="An international, open-source team project focused on redesigning 
+          the website of SAFE Gardening, a nonprofit. Used HTML, CSS, and JavaScript
           to create a better UI/UX."
           source="https://github.com/chingu-voyage4/Geckos-Team-15"
           live="https://chingu-voyage4.github.io/Geckos-Team-15/"
