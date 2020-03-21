d20f6d37eb8050f7eb3589c1af1da26d36399988
beboptank <beboptank12@gmail.com>
2020-03-21 18:51:14 +0900

    add padding to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <div className="footercontainer">
             <p>
-                &copy; 2020{new Date().getFullYear() > 2020 && document.write("-" + new Date().getFullYear())}, Seth Christmus. Made with ❤️, ☕, and create-react-app
+                &copy; 2020{new Date().getFullYear() > 2020 && document.write("-" + new Date().getFullYear())}, Seth Christmus. Made with ❤️, ☕, and React.
             </p>
             
         </div>
