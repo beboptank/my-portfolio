aea9f3088837f8cc7cc9993c78941eec1aac64c3
beboptank <beboptank12@gmail.com>
2020-03-27 23:17:51 +0900

    added component for technologies section

---
+++ src/components/Tech/Tech.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitterSquare, faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+import '../../sass/components/_tech.scss';
+
+
+const Tech = () => {
+    return (
+
+    );
+};
+
+export default Tech;
