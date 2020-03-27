3ba79eac2c4eedf3df6c36713d1c08c2be8bdf09
beboptank <beboptank12@gmail.com>
2020-03-27 23:26:14 +0900

    add initial styling to tech component

--- src/components/Tech/Tech.js
+++ src/components/Tech/Tech.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitterSquare, faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faJsSquare } from '@fortawesome/free-brands-svg-icons';
 
 import '../../sass/components/_tech.scss';
 
 
 const Tech = () => {
     return (
-
+        <section>
+            <div className="techcontainer">
+                <h1>Technologies</h1>
+                <ul className="techcontainer__techlist">
+                    <li><FontAwesomeIcon icon={faJsSquare}/></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </section>
     );
 };
 
