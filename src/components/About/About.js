da30909fb8dd4379bd197a896e870acf742f75e6
beboptank <beboptank12@gmail.com>
2020-04-16 23:37:38 +0900

    Update about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,8 +14,8 @@ const About = () => {
                 <h1 className="aboutcontainer__contentcontainer__head">About Me</h1>
                 <p className="aboutcontainer__contentcontainer__info">
                     I'm a self-taught developer from South Carolina. I've spent the 
-                    past three years learning to code and have provided
-                    front end code for open source projects. Now I'm looking for my 
+                    past three years learning about front end development and have 
+                    worked on multiple open source team projects. Now I'm looking for my 
                     first professional opportunity.
                 </p>
                 <a href="#projects" className="aboutcontainer__contentcontainer__linkbutton">My Projects</a>
