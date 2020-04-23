82bb48a2353ee3d91411ddd8c599ea689b71ee8e
beboptank <beboptank12@gmail.com>
2020-04-23 20:44:34 +0900

    add ProjectShowcase component for voyage 4, edit woody project showcase component

--- src/App.js
+++ src/App.js
@@ -33,10 +33,18 @@ function App() {
         live="https://redditclone-v13.now.sh/"
         />
         <ProjectShowcase 
+          name="SAFE Gardening UI/UX"
+          description="An international, open-source team project focused on 
+          remaking the UI and UX of the website for SAFE Gardening, a non-profit organization. 
+          Used HTML, SCSS, and vanilla JavaScript."
+        source="https://github.com/chingu-voyage4/Geckos-Team-15"
+        live="https://chingu-voyage4.github.io/Geckos-Team-15/"
+        />
+        <ProjectShowcase 
           name="Woody"
           description="Developing and deploying a new website for Woody, a bar in
-          Fukui, Japan. Collaborated with the owner to create a website that
-          reflects the style of his bar. Used Gatsby, React, and SCSS."
+          Fukui, Japan. Collaborating with the owner to create a website that
+          reflects the style of his bar. Using React and SCSS."
           source="https://github.com/beboptank/woody-website"
           live="http://woody.surge.sh/"
         />
