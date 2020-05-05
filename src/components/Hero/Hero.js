253fe60412ee8554f17fd43cd720c761a8649215
beboptank <beboptank12@gmail.com>
2020-05-05 22:20:22 +0900

    add particles.js react component to hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Animated} from 'react-animated-css';
+import Particles from 'react-particles-js';
 
 import '../../sass/components/_hero.scss';
 
@@ -8,6 +9,22 @@ import '../../sass/components/_hero.scss';
 const Hero = () => {
     return (
         <div id="hero" className="herocontainer subsection-layout-flex">
+            <Particles
+                className="particles"
+                params={{
+                    particles: {
+                        number: {
+                            value: 80
+                        },
+                        line_linked: {
+                            shadow: {
+                                enable: true,
+                                color: '#000000',
+                                blur: 3
+                            }
+                        }
+                    }
+                }} />
             <Animated animationIn="bounceInLeft" animationInDuration="1500">
                 <h1 className="herocontainer__head">Hello. My name is Seth Christmus.</h1>
                 <h2 className="herocontainer__subhead">I'm a front end developer.</h2>
