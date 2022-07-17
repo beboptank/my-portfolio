0daef40edb4baf5e707400deee89643b82274c5b
Seth Christmus <seth.christmus@fdmgroup.com>
2022-07-17 17:51:08 -0400

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-# Portfolio - Seth Christmus
-
-This is my first attempt at building a personal portfolio for use when job hunting.
-I hope to turn this into a free template that others can use.
---
+++ precache-manifest.60a321002c4c42c1620eef231ef627d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5f39c0ce819713c408f9d7fab37508",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "f6c9dcfd8f672a2bf573",
+    "url": "/my-portfolio/static/css/2.3ab4c7df.chunk.css"
+  },
+  {
+    "revision": "75eb857e0d32c2a46506",
+    "url": "/my-portfolio/static/css/main.42c646f2.chunk.css"
+  },
+  {
+    "revision": "f6c9dcfd8f672a2bf573",
+    "url": "/my-portfolio/static/js/2.bbe3dd3d.chunk.js"
+  },
+  {
+    "revision": "8e87f68634b1e5b84f6f7c9aeab3aa68",
+    "url": "/my-portfolio/static/js/2.bbe3dd3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75eb857e0d32c2a46506",
+    "url": "/my-portfolio/static/js/main.1e2415a7.chunk.js"
+  },
+  {
+    "revision": "164cd71dc8e56a933ce3",
+    "url": "/my-portfolio/static/js/runtime-main.d96a2d46.js"
+  }
+]);
